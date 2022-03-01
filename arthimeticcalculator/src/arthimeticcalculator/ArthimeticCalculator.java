7a012dd1b9240845e671650c1c8d83f1a22d9021
charishma <kondurucharishma369@gmail.com>
2022-03-01 12:21:44 +0530

    Adding projects

--- arthimeticcalculator/src/arthimeticcalculator/ArthimeticCalculator.java
+++ arthimeticcalculator/src/arthimeticcalculator/ArthimeticCalculator.java
@@ -4,7 +4,7 @@ public class ArthimeticCalculator
 {
 	public static void main(String[] args) {
 		double a, b, ans;
-		char operator;
+		//char operator;
 		int choice;
 		Scanner scan= new Scanner(System.in);
 		System.out.println("Choose an operator : +, -, *, / ");
@@ -13,7 +13,6 @@ public class ArthimeticCalculator
 		System.out.println("3. * ");
 		System.out.println("4. / ");
 		choice = scan.nextInt();
-		
 		if (choice>=1 && choice<=4)
 		{
 			System.out.println("\n Enter any two numbers: ");
@@ -34,4 +33,6 @@ public class ArthimeticCalculator
 		System.out.println("\n Invalid Choice");
 		}
 	}
+
 }
+	
